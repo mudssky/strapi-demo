53acd33324908d7b3d4c86666b11b1978c4162ee
mudssky <mudssky@gmail.com>
2023-10-05 13:24:38 +0800

    ✨ feat: use ts

--- src/api/meeting-room-order/controllers/meeting-room-order.ts
+++ src/api/meeting-room-order/controllers/meeting-room-order.ts
@@ -2,6 +2,13 @@
  * meeting-room-order controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from "@strapi/strapi";
 
-export default factories.createCoreController('api::meeting-room-order.meeting-room-order');
+export default factories.createCoreController(
+  "api::meeting-room-order.meeting-room-order",
+  ({ strapi }) => ({
+    // async create(ctx, next) {
+    // return ctx.badRequest("name 2131321is missing", { foo: "bar" });
+    // },
+  })
+);
